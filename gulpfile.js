a29a43cec7475172abaeb65d222213e10bf3b6ac
Guillermo Snipe <gsnipe@garbarino.com.ar>
2016-05-23 12:39:38 -0300

    Commiting gulp tasks and build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,31 @@ var gulp        = require('gulp'),
     del         = require('del'), // rm -rf
     sass        = require('gulp-sass'),
     fs          = require('fs'),
-    sourcemaps  = require('gulp-sourcemaps');
+    sourcemaps  = require('gulp-sourcemaps'),
+    runSequence = require('run-sequence'),
+    rename      = require('gulp-rename');
 
 
+
+
+gulp.task('delete', function() {
+    return del(['dist']);
+});
 
 gulp.task('sass', function () {
     fs.readFile('APP_VERSION', 'utf8', function (err , data) {
-    return gulp.src('garbaui.scss')
+    return gulp.src('core/garbaui.scss')
+        .pipe(sass())
+        .pipe(rename('garbaui.css'))
+        .pipe(gulp.dest('dist'))
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./'));
+        .pipe(rename('garbaui.min.css'))
+        .pipe(gulp.dest('dist'));
     });
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', function() {
+    runSequence('delete','sass')
+});
