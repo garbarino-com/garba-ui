281c007d60a5a9b94f4c04198d3dbe3fd68dbf6f
Esteban Calvi <estebancalvi@gmail.com>
2017-02-07 11:17:25 -0300

    Linter test config

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+/* eslint-env node */
+/* eslint-disable no-sync */
+'use strict';
+
 var gulp        = require('gulp'),
     del         = require('del'), // rm -rf
     sass        = require('gulp-sass'),
@@ -6,30 +10,47 @@ var gulp        = require('gulp'),
     runSequence = require('run-sequence'),
     rename      = require('gulp-rename');
 
-
-
-
 gulp.task('delete', function() {
-    return del(['dist']);
+  return del(['dist']);
 });
 
 gulp.task('sass', function () {
-    fs.readFile('APP_VERSION', 'utf8', function (err , data) {
-    return gulp.src('core/garbaui.scss')
-        .pipe(sass())
-        .pipe(rename('garbaui.css'))
-        .pipe(gulp.dest('dist/stylesheets'))
-        .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}))
-        .pipe(rename('garbaui.min.css'))
-        .pipe(gulp.dest('dist/stylesheets'));
-    });
+  fs.readFile('APP_VERSION', 'utf8', function (err , data) {
+  return gulp.src('core/garbaui.scss')
+    .pipe(sass())
+    .pipe(rename('garbaui.css'))
+    .pipe(gulp.dest('dist/stylesheets'))
+    .pipe(sass({
+      outputStyle: 'compressed',
+      errLogToConsole: true
+    }))
+    .pipe(
+      rename('garbaui.min.css')
+    )
+    .pipe(
+      gulp.dest('dist/stylesheets')
+    );
+  });
+});
+
+// StyleLint
+gulp.task('stylelint', function lintCssTask() {
+  const gulpStylelint = require('gulp-stylelint');
+
+  return gulp
+    .src('core/**/*.css')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
 });
 
 gulp.task('copyFontsIcon', ['delete'], function() {
-    return gulp.src('./core/fonts/*')
-      .pipe(gulp.dest('dist/fonts'));
+  return gulp.src('./core/fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('default', function() {
-    runSequence('delete','sass', 'copyFontsIcon')
+  runSequence('delete','sass', 'copyFontsIcon')
 });
