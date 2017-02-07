0a529b0b08dd070fabbba13e0a428d34c25f0c3a
Esteban Calvi <estebancalvi@gmail.com>
2017-02-07 12:54:52 -0300

    Basic webpack and react config.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+module.exports = {
+  entry: [
+    './app/js/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: "./js/script.js"
+  },
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new ExtractTextPlugin('./css/garbaui.css', {
+        allChunks: true
+    }),
+  ],
+  watch: true
+};
